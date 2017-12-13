db92051f55aa4c9578ff046e16515694a0ed9572
christiancms <kevin7525>
2017-12-13 20:49:41 -0200

    Inserido a entidade Mesa

---
+++ src/main/resources/META-INF/createDDL_ddlGeneration.sql
@@ -0,0 +1,12 @@
+INSERT INTO status (classe, indice, opcao, versao)
+VALUES
+("Produto",0,"ATIVO",1),
+("Produto",1,"INATIVO",1),
+("Produto",2,"EMFALTA",1),
+("Produto",3,"DEFASADO",1),
+("Pedido",0,"APROVADO",1),
+("Pedido",1,"CANCELADO",1),
+("Pedido",2,"ENTREGUE",1),
+("Pedido",3,"FATURADO",1),
+("Pedido",4,"PENDENTE",1),
+("Pedido",5,"PRONTO",1);
